ada3f51651f68ae1e9012f0edf6df0404fdcadad
pc-home <siyandamakhalima.1@gmail.com>
2018-09-10 06:14:22 +0200

    created the post route for the employee page, user can select specific days

--- src/routes/employeeRoutes.js
+++ src/routes/employeeRoutes.js
@@ -7,7 +7,16 @@ module.exports = function (employee) {
             next(err);
         }
     }
+    async function getData (req, res, next) {
+        try{
+
+            res.render('logged');
+        } catch (err) {
+            next(err);
+        }
+    }
     return {
-        home
+        home,
+        getData
     }
 }
